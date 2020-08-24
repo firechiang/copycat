3f5618e2c31bd4138f08329f661df9afa06eac3d
jiangcihuo <chiang-fire@outlook.com>
2020-08-24 10:35:37 +0800

    fix: 添加相关注释

--- copycat_helloword/src/main/java/com/firechiang/android/copycat_helloword/MainActivity.java
+++ copycat_helloword/src/main/java/com/firechiang/android/copycat_helloword/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * 主界面创建的时候回调
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 设置主界面布局文件
         setContentView(R.layout.activity_main);
     }
 }
