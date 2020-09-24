cf66e05bd56f56bd3c6858d599262726b960b91a
jiangcihuo <chiang-fire@outlook.com>
2020-09-24 17:19:00 +0800

    fix: 添加消息机制（线程间通信）

---
+++ copycat_helloword/src/main/java/com/firechiang/android/copycat_helloword/thread_message/MessageMainActivity.java
@@ -0,0 +1,20 @@
+package com.firechiang.android.copycat_helloword.thread_message;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.firechiang.android.copycat_helloword.R;
+
+/**
+ * 消息机制（线程通信）相关主Activity
+ */
+public class MessageMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thread_message_main_activity);
+    }
+}
