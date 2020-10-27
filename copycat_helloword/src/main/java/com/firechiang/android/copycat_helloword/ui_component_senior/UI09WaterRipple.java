b0653726cc2de4ec651bdd0f88742812fb038c23
jiangcihuo <chiang-fire@outlook.com>
2020-10-27 18:15:46 +0800

    fix: 添加自定义水波纹视图效果

---
+++ copycat_helloword/src/main/java/com/firechiang/android/copycat_helloword/ui_component_senior/UI09WaterRipple.java
@@ -0,0 +1,17 @@
+package com.firechiang.android.copycat_helloword.ui_component_senior;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.firechiang.android.copycat_helloword.R;
+
+/**
+ * 鼠标点击移动有水波纹效果
+ */
+public class UI09WaterRipple extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui_comsenior09_water_ripple);
+    }
+}
