b6a8ee8bcde2cd1409c152413d9af3999a9a1ca2
jiangcihuo <chiang-fire@outlook.com>
2020-10-27 15:46:34 +0800

    fix: 添加自定义开关

---
+++ copycat_helloword/src/main/java/com/firechiang/android/copycat_helloword/ui_component_senior/UI04Switch.java
@@ -0,0 +1,20 @@
+package com.firechiang.android.copycat_helloword.ui_component_senior;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firechiang.android.copycat_helloword.R;
+
+/**
+ * 自定义开关
+ */
+public class UI04Switch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
+        setContentView(R.layout.ui_comsenior04_switch);
+    }
+}
